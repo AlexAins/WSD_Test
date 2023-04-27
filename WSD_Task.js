10413978045fd815ef649c1bb6a5c34ce97a46b5
Alex Ainslie <alex.ainslie99@gmail.com>
2023-04-27 19:13:29 +0100

    Started to solve main question

--- WSD_Task.js
+++ WSD_Task.js
@@ -1,7 +1,22 @@
 function WaterFill(arr, int){
+    // Stating variables
     let bottleSize = arr;
     let taps = int;
-    console.log(bottleSize, taps);
+    let tapQueue = [];
+
+    // Creating element in array for each tap
+    for(let i=0; i<taps; i++){
+        tapQueue.push(0);
+    }
+
+    // Finding smallest Tap Queue 
+    let smallestQueue = tapQueue.indexOf(Math.min(...tapQueue))
+
+    // Now need to use a for loop to add time to each tap in the array, then have next bottle find smallest queue and repeat for all bottles.
+    // After we can simply find highest tap time in tapQueue array to find total time of bottle filling event
+
+    console.log(smallestQueue);
+    console.log(tapQueue);
 };
 
-WaterFill([24,5,6], 2);
+WaterFill([1000,500], 5);
