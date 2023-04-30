468619818f3969c6e0675695402dca6e2020b2dc
Alex Ainslie <alex.ainslie99@gmail.com>
2023-04-30 21:19:42 +0100

    Added FLow function but assuming user know flow rate

--- WSD_Task.js
+++ WSD_Task.js
@@ -1,5 +1,4 @@
 // Main Question
-
 function WaterFill(bottleSize, taps){
 
     // Input Verification
@@ -33,7 +32,6 @@ function WaterFill(bottleSize, taps){
 
 
 // Added Time to walk to Tap (Assuming people at start at Queue)
-
 function WalkWaterFill(bottleSize, taps){
 
     // Input Verification
@@ -67,5 +65,46 @@ function WalkWaterFill(bottleSize, taps){
 
 }
 
+
+// Different Flows (Assuming user knows flow of each tap)
+function FlowWaterFill(bottleSize, taps, tapFlowRate){
+
+    // Input Verification
+    if(bottleSize.length < 1 || !Array.isArray(bottleSize) || !bottleSize.every(num => Number.isInteger(num))){
+        throw new Error('Invalid Input: bottleSize has to be an array of non-negative numbers')
+    }
+    
+    if(taps < 1 || typeof(taps) !== 'number'){
+        throw new Error('Invalid Input: taps has to be a positive integer')
+    }
+
+    if(tapFlowRate.length < 1 || !Array.isArray(tapFlowRate) || !tapFlowRate.every(num => Number.isInteger(num))){
+        throw new Error('Invalid Input: tapFlowRate has to be an array of non-negative numbers')
+    }
+
+
+    // Stating variables
+    let tapQueue = [];
+    let walkTime = 0;
+
+    // Creating element in array for each tap
+    for(let i=0; i<taps; i++){
+        tapQueue.push(0);
+    }
+
+    // For Loop to add time for each water bottle to array of taps
+    for(let j=0; j<bottleSize.length; j++){
+        let smallestQueue = tapQueue.indexOf(Math.min(...tapQueue));
+        tapQueue[smallestQueue] += walkTime + bottleSize[j]/tapFlowRate[smallestQueue];
+    }
+
+    // Finding tap with highest time which will be total time
+    let totalTime = Math.max(...tapQueue)
+
+    console.log("The total time to fill all water bottles is: " + totalTime + " seconds");
+
+}
+
 WaterFill([1000,500,250,600,150], 2);
 WalkWaterFill([1000,500,250,600,150], 2);
+FlowWaterFill([1000,500,250,600,150],2,[200,100])
